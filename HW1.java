c4e1aad39610ab2ab0e530ab744eedb9967e783f
Andrew Ho <ash023@ucsd.edu>
2017-04-09 15:09:31 -0700

    Random Matrix Generation

--- HW1.java
+++ HW1.java
@@ -1,5 +1,6 @@
 import java.util.HashSet;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class HW1
 {
@@ -29,6 +30,49 @@ public class HW1
       return new Integer(x).hashCode() * 31 + new Integer(y).hashCode();
     }
 
+  }
+  
+  public void randMatrix(int [][] arr)
+  {
+    Random rand = new Random();
+
+    int index = 1;
+    for (int i = 0; i < arr.length; i++)
+    {
+      for (int j = 0; j < index; j++)
+      {
+        if (i == j)
+        {
+          arr[i][j] = 0;
+          continue;
+        }
+        
+        int val = rand.nextInt(2);
+        if (val == 1)
+        {
+          arr[i][j] = val;
+          arr[j][i] = val;
+        }
+        else
+        {
+          arr[i][j] = val;
+          arr[i][j] = val;
+        }
+      }
+      index++;
+    }   
+  }
+  
+  private void printMatrix(int [][] arr)
+  {
+    for (int i = 0; i < arr.length; i++)
+    {
+      for (int j = 0; j < arr.length; j++)
+      {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.print("\n");
+    }
   }
   
   public  boolean existTriangle(int [][] arr)
@@ -86,7 +130,12 @@ public class HW1
     
     int [][] arr1 = new int[][] { {0,1,0,0,0}, {1,0,1,1,0}, {0,1,0,1,1}, {0,1,1,0,1}, {0,0,1,1,0}};
     
-    
+    int [][] arr2 = new int[(int)Math.pow(2,6)][(int)Math.pow(2,6)];
+    hw.randMatrix(arr2);
+
+    System.out.println(hw.existTriangle(arr2));
+
+
     System.out.println(hw.existTriangle(arr));
     System.out.println(hw.existTriangle(arr1));
   }
